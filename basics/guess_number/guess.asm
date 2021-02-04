ee6793d7e2b45a8649b004d23fe44d6c641f833a
Wiktor ZAając <wiktor.zajac888@gmail.com>
2021-02-04 02:04:04 +0100

    take input and print it, bad var declartation

---
+++ basics/guess_number/guess.asm
@@ -0,0 +1,26 @@
+global _start
+
+section .text:
+_start:
+    mov eax, 0x3
+    mov ebx, 0
+    mov ecx, $message
+    mov edx, message_length
+    int 0x80
+    mov eax, 0x4
+    mov ebx, 1
+    mov ecx, message
+    mov edx, message_length
+    int 0x80
+
+exit:
+    mov eax, 0x1
+    mov ebx, 0
+    int 0x80
+
+section .data 
+    message: db "number"
+    message_length equ $-message
+
+  
+
