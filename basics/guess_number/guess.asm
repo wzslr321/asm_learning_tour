60700f4b731c1b43a11c8e4b2f71e2d5315a89fd
Wiktor ZAając <wiktor.zajac888@gmail.com>
2021-02-04 19:28:07 +0100

    proper comparison

--- basics/guess_number/guess.asm
+++ basics/guess_number/guess.asm
@@ -1,61 +1,64 @@
 section .bss
-    number resb 16
+    password resb 8
 
 section .data 
-    message: db "Guess a number from 1-9!", 0xA
+    message: db "Enter a password of 8 characters !", 0xA
     message_length equ $-message
 
-    cb_message: db "Your guess is: "
-    cb_message_length equ $- cb_message
+    correct_password: db "torvalds"
+    correct_password_length equ $-correct_password
 
-    win_result_message: db "Congratulations, you guessed the number !", 0xA
-    win_result_message_length equ $- win_result_message
+    success_message: db "Congratulations, welcome!", 0xA
+    success_message_length equ $-success_message
 
-global _start
+    fail_message: db "Unfortunetely, you aren't welcome here!", 0xA
+    fail_message_length equ $-fail_message
+
 
 section .text:
+    global _start
 _start:
-    call _showInformation
-    call _showCbMsg
-    call _showNumber
-    call _winMsg
 
-_showInformation:
-    mov eax, 0x4
-    mov ebx, 1
-    mov ecx, message
-    mov edx, message_length
-    int 0x80
+    _showInformation:
+        mov eax, 0x4
+        mov ebx, 1
+        mov ecx, message
+        mov edx, message_length
+        int 0x80
 
-_getNumber:
-    mov eax, 0x3
-    mov ebx, 0
-    mov ecx, number
-    mov edx, 1
-    int 0x80
+    _getPassword:
+        mov eax, 0x3
+        mov ebx, 0
+        mov ecx, password
+        mov edx, 8
+        int 0x80
 
-_showCbMsg:
-    mov eax, 0x4
-    mov ebx, 1
-    mov ecx, cb_message 
-    mov edx, cb_message_length 
-    int 0x80
+    _checkPassword:
+        mov eax, [password]
+        cmp eax, [correct_password]
+        jne else
+        jmp _successMsg
+        else:
+            call _failMsg
 
-_showNumber:
-    mov eax, 0x4
-    mov ebx, 1
-    mov ecx, number
-    mov edx, 1
-    int 0x80
+    _failMsg:
+        mov eax, 0x4
+        mov ebx, 1
+        mov ecx, fail_message
+        mov edx, fail_message_length            
+        int 0x80
+        jmp exit
 
-_winMsg: 
-    mov eax, 0x4
-    mov ebx, 1
-    mov ecx, win_result_message
-    mov edx, win_result_message_length
-    int 0x80
+    _successMsg:
+        mov eax, 0x4
+        mov ebx, 1
+        mov ecx, success_message
+        mov edx, success_message_length
+        int 0x80
 
 exit:
     mov eax, 0x1
     mov ebx, 0
     int 0x80
+
+
