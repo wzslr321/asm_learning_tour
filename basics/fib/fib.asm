5f20e8ee32855f6ff991aaaba67b8b16e5925d65
Wiktor ZAając <wiktor.zajac888@gmail.com>
2021-02-16 00:39:38 +0100

    code cleanup

--- basics/fib/fib.asm
+++ basics/fib/fib.asm
@@ -1,4 +1,3 @@
- section .bss
 section .bss
     num resb 1
 
@@ -24,37 +23,37 @@ _start:
         push ax; push ax on the stack; ax = '0' ; it secures from being overwritten by int0x80
         push dx; push dx on the stack; ax = '1'
 
-    _increase:
+    _printNum:
         mov eax, 0x4
         mov ebx, 1
         mov ecx, num ; print num
         mov edx, 1
         int 0x80
 
-
+    _popValues:
         pop dx ; copy top of the stack (dx); dx = '1'
         pop ax ; copy top of the stack (ax); ax = '0'
 
-        ; int 3 ; debugger shows the same values as before, 0x30 and 0x31.
+    ; int 3 ; debugger shows the same values as before, 0x30 and 0x31.
         
-        sub dx, 30h; ebx = 1; ebx = 1; ebx = 2; ebx = 3
-        sub ax, 30h; eax = 0; eax = 1; eax = 1; eax = 2
-
-        ; sub 0x30 from dx, and ax to make it possible to add them
+    sub dx, 30h; ebx = 1; 
+    sub ax, 30h; eax = 0; 
+    ; sub 0x30 from dx, and ax to make it possible to add them
     
-        ; int 3
-        ; after substraction, debugger shows properly its values as integers - 0x0 and 0x1
+    ; int 3
+    ; after substraction, debugger shows properly its values as integers - 0x0 and 0x1
 
-        add ax, dx; eax = 1; eax = 2; eax = 3; eax = 5
-        mov [num], ax; [sum] = 1; [sum] = 2; sum = 3; [sum] = 5
+    _alghoritm:
+        add ax, dx; eax = 1; 
+        mov [num], ax; [num] = 1; 
 
-        mov ax, dx;  eax = 1; eax = 1; eax = 2; eax = 3
-        add ax, 30h ; eax = '1'; eax = '1'; eax = '2'; eax = '3'
-        mov dx, [num] ; ebx = 1; ebx = 2; ebx = 3; ebx = 5 
-        add dx, 30h; ebx = '1'; ebx = '2'; ebx = '3'; ebx = '5'
+        mov ax, dx;  eax = 1; 
+        add ax, 30h ; eax = '1';
+        mov dx, [num] ; ebx = 1; 
+        add dx, 30h; ebx = '1'; 
 
-        pop ecx
-        loop _repeat
+    pop ecx
+    loop _repeat
 
     _newLine:
       mov eax, 0x4
