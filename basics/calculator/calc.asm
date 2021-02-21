a96d6313e2147cf1c8dfbb6403e6891e092ac981
Wiktor ZAając <wiktor.zajac888@gmail.com>
2021-02-21 23:44:22 +0100

    proper comparison test

--- basics/calculator/calc.asm
+++ basics/calculator/calc.asm
@@ -9,6 +9,18 @@ section .data
 
     num_msg: db "Enter first number", 0xA
     num_msg_length equ $- num_msg
+    
+    add_msg: db "Add", 0xA
+    add_msg_length equ $- add_msg 
+    
+    sub_msg: db "Substract", 0xA
+    sub_msg_length equ $- sub_msg
+
+    mul_msg: db "Multiply", 0xA
+    mul_msg_length equ $- mul_msg
+    
+    div_msg: db "Divide", 0xA
+    div_msg_length equ $- div_msg
 
     newLine: db 0xA
 
@@ -52,12 +64,20 @@ _start:
         je _sub
 
     _add:
+      call _addMessage
+      jmp exit
 
     _sub:
+      call _subMessage
+      jmp exit
 
     _mul:
+      call _mulMessage
+      jmp exit
     
     _div:
+      call _divMessage
+      jmp exit
 
     _printNum:
 
@@ -80,6 +100,38 @@ section .text
       mov ebx, 1
       mov ecx, num_msg
       mov edx, num_msg_length
+      int 0x80
+      ret
+
+    _addMessage:
+      mov eax, 0x4
+      mov ebx, 1
+      mov ecx, add_msg
+      mov edx, add_msg_length
+      int 0x80
+      ret
+
+    _subMessage:
+      mov eax, 0x4
+      mov ebx, 1
+      mov ecx, sub_msg
+      mov edx, sub_msg_length
+      int 0x80
+      ret
+
+    _mulMessage:
+      mov eax, 0x4
+      mov ebx, 1
+      mov ecx, mul_msg
+      mov edx, mul_msg_length
+      int 0x80
+      ret
+
+    _divMessage:
+      mov eax, 0x4
+      mov ebx, 1
+      mov ecx, div_msg
+      mov edx, div_msg_length
       int 0x80
       ret
 
